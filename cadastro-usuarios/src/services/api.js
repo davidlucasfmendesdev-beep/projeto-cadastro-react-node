c5ffdca2715fb2b589dd923a2dbddfd99d12bde9
david <davidlucasfmendesdev@gmail.com>
2026-08-09 14:10:16 -0300

    fix: adicionando https na url da api

--- cadastro-usuarios/src/services/api.js
+++ cadastro-usuarios/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://projeto-cadastro-react-node.onrender.com/' // <-- O seu link do Render aqui!
+  baseURL: 'https://projeto-cadastro-react-node.onrender.com/' // <-- O seu link do Render aqui!
 })
 
 export default api
